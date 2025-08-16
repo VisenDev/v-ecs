f8508cc656810925d8d08d3f360cbf8b352af903
Robert Burnett <dr.slate2@gmail.com>
2025-08-15 22:15:28 -0500

    minor improvements

--- main.c
+++ main.c
@@ -8,20 +8,24 @@ typedef struct {
 
 typedef struct {
     const char * name;
-    ecs_Value default_value;
-} ecs_FieldDef;
+    ecs_Value value;
+} ecs_Field;
 
-#define ECS_COMPONENTDEF_MAX_FIELDS 16
+#define ECS_COMPONENT_MAX_FIELDS 16
 typedef struct {
     const char * name;
-    ecs_FieldDef fields[ECS_COMPONENTDEF_MAX_FIELDS];
-} ecs_ComponentDef;
+    ecs_Field fields[ECS_COMPONENT_MAX_FIELDS];
+} ecs_Component;
+
+#define ecs_Vec(Type) struct { Type * items; int len; int cap; }
+
+typedef ecs_Vec(ecs_Component) ecs_Components;
+typedef ecs_Vec(int) ecs_Ids;
 
 typedef struct {
-    ecs_ComponentDef * component_defs;
-    int component_defs_len;
-    int component_defs_cap;
-    
+    ecs_Components prototypes;
+    ecs_Components components;
+    ecs_Ids ids;
 } ecs_Ecs;
 
 //void ecs_define_component(ecs_Ecs * e, const char * name, I/
@@ -30,12 +34,16 @@ int * ecs_query(const ecs_Ecs *, const char *);
 int game_do_physics(void) {
     int * iter = ecs_query(0, "physics");
     while(iter) {
-        ecs_ComponentDef comp = ecs_get_component(&ecs, *iter, "physics");
+        ecs_Component comp = ecs_get_component(&ecs, *iter, "physics");
     }
 }
 
+void ecs_component_prototype(ecs_Ecs * e, ecs_Component component) {
+
+}
+
 int main() {
-    ecs_ComponentDef physics = {
+    ecs_Component physics = {
         .name = "physics",
         .fields = {
             {.name = "x", .default_value = {0}},
