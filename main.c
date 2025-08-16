09aa6b9dcc634b9448fcee7969a5219aa00cd56d
Robert Burnett <dr.slate2@gmail.com>
2025-08-15 22:39:20 -0500

    data storage improvements

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 typedef struct {
     int tag;
     union {
-        int data;
+        double data; //a double can store a normal 32 bit int without rounding!
         void * ptr;
     };
 } ecs_Value;
@@ -18,14 +22,29 @@ typedef struct {
 } ecs_Component;
 
 #define ecs_Vec(Type) struct { Type * items; int len; int cap; }
+#define ecs_vec_append(vec, value) do { \
+    if((vec)->cap <= 0) { \
+        (vec)->cap = 8; \
+        (vec)->len = 0; \
+        (vec)->items = malloc(sizeof((vec)->items[0]) * (vec)->cap); \
+        assert((vec)->items); \
+    } else if((vec)->len + 1 >= (vec)->cap) { \
+        (vec)->cap = (vec)->cap * 2; \
+        (vec)->items = malloc(sizeof((vec)->items[0]) * (vec)->cap); \
+        assert((vec)->items); \
+    } \
+    (vec)->items[(vec)->len++] = value; \
+} while (0)
 
+typedef struct { int i; } ecs_Id;
 typedef ecs_Vec(ecs_Component) ecs_Components;
-typedef ecs_Vec(int) ecs_Ids;
+typedef ecs_Vec(ecs_Id) ecs_Ids;
 
 typedef struct {
     ecs_Components prototypes;
     ecs_Components components;
-    ecs_Ids ids;
+    ecs_Id id_cap;
+    ecs_Ids free_ids;
 } ecs_Ecs;
 
 //void ecs_define_component(ecs_Ecs * e, const char * name, I/
@@ -38,23 +57,35 @@ int game_do_physics(void) {
     }
 }
 
-void ecs_component_prototype(ecs_Ecs * e, ecs_Component component) {
+void ecs_component_define_prototype(ecs_Ecs * e, ecs_Component prototype) {
+    ecs_vec_append(&e->prototypes, prototype);
+}
 
+ecs_Id ecs_entity_new(ecs_Ecs * e) {
+    if(e->free_ids.len <= 0) {
+        ecs_Id stop = {e->id_cap.i + 128};
+        for(; e->id_cap.i < stop.i; ++e->id_cap.i) {
+            ecs_vec_append(&e->free_ids, e->id_cap);
+        }
+    }
+    return e->free_ids.items[--e->free_ids.len];
 }
 
 int main() {
-    ecs_Component physics = {
+    ecs_Ecs  e = {0};
+    ecs_component_define_prototype(&e, (ecs_Component){
         .name = "physics",
         .fields = {
-            {.name = "x", .default_value = {0}},
-            {.name = "y", .default_value = {0}},
-            {.name = "x_vel", .default_value = {0}},
-            {.name = "y_vel", .default_value = {0}},
-            {.name = "x_acc", .default_value = {0}},
-            {.name = "y_acc", .default_value = {0}},
+            {.name = "x", .value = {0}},
+            {.name = "y", .value = {0}},
+            {.name = "x_vel", .value = {0}},
+            {.name = "y_vel", .value = {0}},
+            {.name = "x_acc", .value = {0}},
+            {.name = "y_acc", .value = {0}},
         }
-    };
+    });
 
+    ecs_Id new = ecs_entity_new(&e);
 
 }
 
