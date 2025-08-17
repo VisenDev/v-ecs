8369c5e96d49287e8145a7be852e7d2a1ff5897c
Robert Burnett <dr.slate2@gmail.com>
2025-08-17 17:03:16 -0500

    more work on parser

--- parser.c
+++ parser.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
+#include <assert.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <string.h>
+
+//typedef struct {
+//    enum {
+//        ECS_PARSER_TYPE_INT,
+//        ECS_PARSER_TYPE_FLOAT,
+//        ECS_PARSER_TYPE_CUSTOM,
+//} ecs_parser_Type;
+
+typedef enum {
+    ECS_PARSER_TYPE_INT = 0,
+    ECS_PARSER_TYPE_LONG,
+    ECS_PARSER_TYPE_SHORT,
+    ECS_PARSER_TYPE_CHAR,
+    ECS_PARSER_TYPE_BOOL,
+    ECS_PARSER_TYPE_FLOAT,
+    ECS_PARSER_TYPE_DOUBLE = 255,
+
+    //Any types greater than 255 are ids
+} ecs_parser_Type;
 
 typedef struct {
-    const char * name;
-    //*type information*/
-} ecs_Type;
+    const char name[32];
+    ecs_parser_Type type;
+} ecs_parser_StructField;
 
 typedef struct {
-    int len;
-    int cap;
-    ecs_Type * types; 
+    const char * name[32];
+    ecs_parser_StructField fields[32];
+} ecs_parser_Struct;
+
+typedef struct {
+    int count;
+    ecs_parser_Struct structs[32];
 
     FILE * fp;
 } ecs_Parser;
@@ -34,14 +59,56 @@ bool ecs_parser_expect(ecs_Parser * p, const char * str) {
     return true;
 }
 
-ecs_Type ecs_parser_parse_type(ecs_Parser * p) {
-    
+
+bool ecs_parser_is_identifier_char(char ch) {
+    return ch == '_' || isalpha(ch) || isdigit(ch);
 }
+
+bool ecs_parser_read_identifier(ecs_Parser * p, char * dst, size_t size) {
+    ecs_parser_skip_whitespace(p);
+    if(ecs_parser_peek(p) != '_' && !isalpha(ecs_parser_peek(p))) {
+        return false;
+    }
+    int i = 0;
+    while(ecs_parser_is_identifier_char(ecs_parser_peek(p))) {
+       if(i + 2 > size) return false;
+       dst[i++] = fgetc(p->fp);
+       dst[i] = 0;
+    }
+    return true;
+}
+
+#define streql(a, b) (strcmp(a, b) == 0)
+
+bool ecs_parser_type_from_name(ecs_Parser * p, char * name, ecs_parser_Type * out) {
+    if(streql(name, "int")) {
+        *out = ECS_PARSER_TYPE_INT;
+    } else if(streql(name, "long")) {
+        *out = ECS_PARSER_TYPE_LONG;
+    } else {
+        //TODO lookup name in parser list of structs
+        return false;
+    }
+
+    return true;
+}
+
+bool ecs_parser_parse_type(ecs_Parser * p, ecs_parser_Type * out) {
+    char name[1024] = {0};
+    if(!ecs_parser_read_identifier(p, name, sizeof(name))) return false;
+
+    if(!ecs_parser_type_from_name(p, name, out)) return false;
+    return true;
+}
+
 
 void ecs_parser_parse_struct_field(ecs_Parser * p) {
     char buf[1024] = {0};
+    ecs_parser_Type t = {0};
+    if(!ecs_parser_parse_type(p, &t)) assert(0);
 
-
+    //TODO parse name;
+    //
 }
 
 void ecs_parser_parse_struct(ecs_Parser * p) {
